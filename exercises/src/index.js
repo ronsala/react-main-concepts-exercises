5b75ab7f4a6acc132b5367a6be51a15c4345ea68
RonSala <ron.sala@gmail.com>
2021-08-20 14:20:13 -0400

    implement a version of 'Hello, world'

--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import HelloWorld from './HelloWorld';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
